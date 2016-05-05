7051cdbd09de816a22eb25e5b49463b2132f2652
karengh <ghazaryan.karen@gmail.com>
2016-05-05 17:05:19 +0400

    add compass process

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,18 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
-    browserify = require('gulp-browserify');
+    browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
     concat = require('gulp-concat');
 
 var jsSources = [
     'components/scripts/rclick.js',
     'components/scripts/pixgrid.js',
     'components/scripts/tagline.js',
-    'components/scripts/template.js',
+    'components/scripts/template.js'
 ];
+
+var sassSourcess = ['components/sass/style.scss'];
 
 gulp.task('coffee', function() {
     gulp.src ('components/coffee/tagline.coffee')
@@ -25,3 +28,13 @@ gulp.task('js', function() {
         .pipe(gulp.dest('builds/development/js'))
 });
 
+gulp.task('compass', function() {
+    gulp.src(sassSourcess)
+        .pipe(compass({
+            sass: 'components/sass',
+            images: 'builds/development/images',
+            style: 'expanded'
+        }))
+        .on('error', gutil.log)
+        .pipe(gulp.dest('builds/development/css'))
+});
