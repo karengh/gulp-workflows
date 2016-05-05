d587aac0a39aa4fe71a39d396239a6ebeea19e02
karengh <ghazaryan.karen@gmail.com>
2016-05-05 20:01:54 +0400

    add local server and live reload

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ var gulp = require('gulp'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
     compass = require('gulp-compass'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    connect = require('gulp-connect')
 
 var coffeeSources = 'components/coffee/tagline.coffee';
 
@@ -28,6 +29,7 @@ gulp.task('js', function() {
         .pipe(concat('script.js'))
         .pipe(browserify())
         .pipe(gulp.dest('builds/development/js'))
+        .pipe(connect.reload())
 });
 
 gulp.task('compass', function() {
@@ -39,6 +41,14 @@ gulp.task('compass', function() {
         }))
         .on('error', gutil.log)
         .pipe(gulp.dest('builds/development/css'))
+        .pipe(connect.reload());
+});
+
+gulp.task('connect', function() {
+    connect.server({
+        root: 'builds/development/',
+        livereload: true
+    });
 });
 
 gulp.task('watch', function() {
@@ -47,4 +57,4 @@ gulp.task('watch', function() {
     gulp.watch('components/sass/*.scss', ['compass']);
 });
 
-gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
+gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
